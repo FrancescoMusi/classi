8a85f2f9022cefd1fd65533b23292e7e3712e305
Francesco Musi <francesco.musi03@gmail.com>
2019-02-03 14:14:20 +0100

    bug fixed & boss movement

--- main.py
+++ main.py
@@ -17,15 +17,18 @@ class Gold():
 
 class Player():
 	def __init__(self, hp):
-		self.cur_x = random.randint(0, field.x-1)
-		self.cur_y = random.randint(0, field.y-1)
+		while True:
+			self.cur_x = random.randint(0, field.x-1)
+			self.cur_y = random.randint(0, field.y-1)
 
-		self.cur_hp = hp
+			if self.cur_x != gold.x and self.cur_y != gold.y:
+				break
+
 		self.max_hp = hp
+		self.cur_hp = hp
 
 	def printStatus(self):
 		print("player hp: {}%".format(player.cur_hp))
-
 	
 	def move(self, direction):
 		if direction == 'w' and player.cur_x >= 1:
@@ -41,8 +44,13 @@ class Player():
 
 class Enemy():
 	def __init__(self, damage):
-		self.cur_x = random.randint(0, field.x-1)
-		self.cur_y = random.randint(0, field.y-1)
+		while True:
+			self.cur_x = random.randint(0, field.x-1)
+			self.cur_y = random.randint(0, field.y-1)
+
+			if self.cur_x != gold.x and self.cur_y != gold.y and self.cur_x != player.cur_x and self.cur_y != player.cur_y:
+				break
+
 		self.damage = damage
 
 	def checkEnemyOnPlayer(self):
@@ -53,20 +61,67 @@ class Enemy():
 
 
 	def move(self):
-		if self.cur_x > player.cur_x and self.cur_x > 0 and	self.cur_x-1 != ['G']:
+		if self.cur_x > player.cur_x and self.cur_x > 0 and	self.cur_x-1 != ['G'] and	self.cur_x-1 != ['M'] and	self.cur_x-1 != ['B']:
 			self.cur_x -= 1
-		elif self.cur_y > player.cur_y and self.cur_y > 0 and	self.cur_y-1 != ['G']:
+		elif self.cur_y > player.cur_y and self.cur_y > 0 and	self.cur_y-1 != ['G'] and	self.cur_y-1 != ['M'] and	self.cur_y-1 != ['B']:
 			self.cur_y -= 1
-		elif self.cur_x < player.cur_x and self.cur_x < field.x-1 and	self.cur_x+1 != ['G']:
+		elif self.cur_x < player.cur_x and self.cur_x < field.x-1 and	self.cur_x+1 != ['G'] and	self.cur_x+1 != ['M'] and	self.cur_x+1 != ['B']:
 			self.cur_x += 1
-		elif self.cur_y < player.cur_y and self.cur_y < field.y-1 and	self.cur_y+1 != ['G']:
+		elif self.cur_y < player.cur_y and self.cur_y < field.y-1 and	self.cur_y+1 != ['G'] and	self.cur_y+1 != ['M'] and	self.cur_y+1 != ['B']:
 			self.cur_y += 1
-	
+	 
 
 	def attack(self, target):
 		target.cur_hp -= self.damage
 		self.cur_x = random.randint(0, field.x-1)
 		self.cur_y = random.randint(0, field.y-1)
+
+
+class Boss(Enemy):
+	def __init__(self, damage):
+		self.cur_x = random.randint(0, field.x-1)
+		self.cur_y = random.randint(0, field.y-1)
+		self.damage = damage
+
+		Enemy.__init__(self, damage)
+
+	def move(self):
+		# x>  y>
+		if self.cur_x > player.cur_x and self.cur_y > player.cur_y and	self.cur_x-1 != ['G'] and	self.cur_x-1 != ['M'] and	self.cur_x-1 != ['B']:
+			self.cur_x -= 1
+			self.cur_y -= 1
+
+		# x<  y>
+		elif self.cur_y > player.cur_y  and self.cur_x < player.cur_x  and	self.cur_y-1 != ['G'] and	self.cur_y-1 != ['M'] and	self.cur_y-1 != ['B']:
+			self.cur_y -= 1
+			self.cur_x += 1
+
+		# x>  y<
+		elif self.cur_x > player.cur_x and self.cur_y < player.cur_y and	self.cur_x+1 != ['G'] and	self.cur_x+1 != ['M'] and	self.cur_x+1 != ['B']:
+			self.cur_x -= 1
+			self.cur_y += 1
+
+		# x<  y<
+		elif self.cur_y < player.cur_y  and self.cur_x < player.cur_x and  self.cur_y+1 != ['G'] and	self.cur_y+1 != ['M'] and	self.cur_y+1 != ['B']:
+			self.cur_y += 1
+			self.cur_x += 1
+
+		#normal move
+		elif self.cur_x > player.cur_x and self.cur_x > 0 and	self.cur_x-1 != ['G'] and	self.cur_x-1 != ['M'] and	self.cur_x-1 != ['B']:
+			self.cur_x -= 1
+
+		elif self.cur_y > player.cur_y and self.cur_y > 0 and	self.cur_y-1 != ['G'] and	self.cur_y-1 != ['M'] and	self.cur_y-1 != ['B']:
+			self.cur_y -= 1
+
+		elif self.cur_x < player.cur_x and self.cur_x < field.x-1 and	self.cur_x+1 != ['G'] and	self.cur_x+1 != ['M'] and	self.cur_x+1 != ['B']:
+			self.cur_x += 1
+			
+		elif self.cur_y < player.cur_y and self.cur_y < field.y-1 and	self.cur_y+1 != ['G'] and	self.cur_y+1 != ['M'] and	self.cur_y+1 != ['B']:
+			self.cur_y += 1
+ 
+
+
+
 
 class Field():
 	def __init__(self, x, y):
@@ -81,25 +136,17 @@ class Field():
 				self.graphic_field[i].append([' '])
 
 	def printField(self):
-		while True:
 			self.graphic_field[player.cur_x][player.cur_y] = ['P']
 			self.graphic_field[monster1.cur_x][monster1.cur_y] = ['M']
 			self.graphic_field[monster2.cur_x][monster2.cur_y] = ['M']
 			self.graphic_field[boss.cur_x][boss.cur_y] = ['B']
 			self.graphic_field[gold.x][gold.y] = ['G']
 
-			self.counter = 0 
-			for i in self.graphic_field:
-				if i != [' ']:
-					self.counter += 1
-
-			if self.counter >= 4:
-				for y in self.graphic_field:
-					for x in y:
-						print(x, end = '')
-					print()
-				break
-
+			for y in self.graphic_field:
+				for x in y:
+					print(x, end = '')
+				print()
+			
 
 
 
@@ -122,18 +169,37 @@ def printLose():
 		print('reastart in {}...'.format(3-(i+1)))
 		time.sleep(1)
 		os.system('cls')
+		player.cur_hp = player.max_hp
 
-#ISTANZE
+#istanze
 field = Field(10, 10)
+gold = Gold()
+player = Player(100)
 monster1 = Enemy(50)
 monster2 = Enemy(50)
-boss = Enemy(100)
+boss = Boss(100)
 
-player = Player(100)
-gold = Gold()
 
+#introduzione
 os.system('cls')
 
+print('''Ci  saranno  3 nemici,  denominati  con
+\'M\' e \'B\'.
+Il boss (B) ti uccide con un colpo solo
+e può muoversi in diagonale.
+Gli altri 2 mostri si possono muovere
+solo a destra, sinistra, in alto oppure
+in basso e ti uccidono con 2 colpi.
+Il tuo obbiettivo è arrivare al tesoro (G).
+
+Muoviti con \'w\', \'a\', \'s\', \'d\'.
+
+-premi invio per continuare- ''')
+
+input()
+
+#mainloop
+os.system('cls')
 while True:
 	while True:
 		field.createField()
@@ -167,3 +233,6 @@ while True:
 			sys.exit()
 
 		os.system('cls')
+
+
+
